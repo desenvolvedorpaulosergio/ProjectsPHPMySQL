400c7966c451de1111a1f9fce601752b75524aa6
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-03 16:57:48 -0300

    envair

---
+++ forms/form1/classjs/index.js
@@ -0,0 +1,4 @@
+function eventoComecar(){
+    window.document.getElementById('config-display-form').style.display = 'block'
+    window.document.getElementById('estilo-apresentacao').style.display = 'none'
+}
