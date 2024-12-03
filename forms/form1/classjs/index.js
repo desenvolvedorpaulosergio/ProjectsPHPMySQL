dcacf1c2c6e25a85fc7aef12c4129aae075dff0b
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-03 17:26:27 -0300

    enviar

--- forms/form1/classjs/index.js
+++ forms/form1/classjs/index.js
@@ -1,4 +1,8 @@
 function eventoComecar(){
-    window.document.getElementById('config-display-form').style.display = 'block'
+    window.document.getElementById('config-display-modal').style.display = 'block'
     window.document.getElementById('estilo-apresentacao').style.display = 'none'
+    setInterval(function(){
+        window.document.getElementById('config-display-form2').style.display = 'block'
+        window.document.getElementById('config-display-modal').style.display = 'none'
+    }, 1500)
 }
