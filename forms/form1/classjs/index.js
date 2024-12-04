9312a7f3d4737f22999a1491e38fd37b3fe37608
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-04 12:38:48 -0300

    Atualizar

--- forms/form1/classjs/index.js
+++ forms/form1/classjs/index.js
@@ -21,9 +21,15 @@ function eventoVerificar(){
     const RegEx_Telefone = /^\([0-9]{2}\) \+[0-9]{2} [0-9]{5}\-[0-9]{4}$/
     const RegEx_CPF = /^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/
 
-    if(RegEx_Gmail.test(gmail) == true && RegEx_CPF.test(cpf) == true && RegEx_Telefone.test(telefone) == true && 
-    nome !== "" && senha !== "" && endereco !== "" && observacao !== ""){
-        alert('ok')
+    if(!(RegEx_Gmail.test(gmail) == true && RegEx_CPF.test(cpf) == true && RegEx_Telefone.test(telefone) == true && 
+    nome !== "" && senha !== "" && endereco !== "" && observacao !== "")){
+        setInterval(function(){
+            window.document.getElementById('estilo-mini-carregamento').style.display = "none"
+            window.document.getElementById('config-display-botao1').style.display = "block"
+        }, 3000)
+        window.document.getElementById('estilo-mini-carregamento').style.display = "block"
+        window.document.getElementById('config-display-botao1').style.display = "none"
+        window.document.getElementById('config-display-botao2').style.display = "none"
     }else{
         alert('erro')
     }
