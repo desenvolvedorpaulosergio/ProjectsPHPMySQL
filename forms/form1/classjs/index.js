8ee0acc12bcdfa095b8f13f7d65ca05846b88b97
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-04 12:16:32 -0300

    Enviar

--- forms/form1/classjs/index.js
+++ forms/form1/classjs/index.js
@@ -6,14 +6,23 @@ window.document.addEventListener('DOMContentLoaded', function(){
 
 function eventoVerificar(){
 
+    let nome = window.document.getElementById('nome').value
+    let senha = window.document.getElementById('senha').value
+    let endereco = window.document.getElementById('endereco').value
+    let observacao = window.document.getElementById('observacao').value
+
     let gmail = window.document.getElementById('gmail').value
     let cpf = window.document.getElementById('cpf').value
     let telefone = window.document.getElementById('telefone').value
+    
 
-    const RegEx_Gmail = /^$/
-    const RegEx_Telefone = /^$/
-    const RegEx_CPF = /^$/
-    if(RegEx_Gmail.test(gmail) == true){
+    // Configurações das Expressões Regulares
+    const RegEx_Gmail = /^[a-z0-9]{3,}@gmail\.com$/
+    const RegEx_Telefone = /^\([0-9]{2}\) \+[0-9]{2} [0-9]{5}\-[0-9]{4}$/
+    const RegEx_CPF = /^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/
+
+    if(RegEx_Gmail.test(gmail) == true && RegEx_CPF.test(cpf) == true && RegEx_Telefone.test(telefone) == true && 
+    nome !== "" && senha !== "" && endereco !== "" && observacao !== ""){
         alert('ok')
     }else{
         alert('erro')
