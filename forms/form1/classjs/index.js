5520747f4274d4826b68e722d759f888362975be
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-04 11:45:21 -0300

    atualizar

--- forms/form1/classjs/index.js
+++ forms/form1/classjs/index.js
@@ -3,3 +3,19 @@ window.document.addEventListener('DOMContentLoaded', function(){
         window.document.getElementById('config-display-container').style.display = 'none'
     }, 1500)
 })
+
+function eventoVerificar(){
+
+    let gmail = window.document.getElementById('gmail').value
+    let cpf = window.document.getElementById('cpf').value
+    let telefone = window.document.getElementById('telefone').value
+
+    const RegEx_Gmail = /^$/
+    const RegEx_Telefone = /^$/
+    const RegEx_CPF = /^$/
+    if(RegEx_Gmail.test(gmail) == true){
+        alert('ok')
+    }else{
+        alert('erro')
+    }
+}
