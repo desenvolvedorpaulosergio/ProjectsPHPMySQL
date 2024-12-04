a73e90d27dbcde4797afd6f6529941ee1a64d86a
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-04 10:53:31 -0300

    ENVIAR

---
+++ forms/form1/classjs/index.js
@@ -0,0 +1,5 @@
+window.document.addEventListener('DOMContentLoaded', function(){
+    setInterval(function(){
+        window.document.getElementById('config-display-container').style.display = 'none'
+    }, 1500)
+})
