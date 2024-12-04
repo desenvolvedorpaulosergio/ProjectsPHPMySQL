adff90d90294ffeca30a7e56ffd39f2e9032e7dd
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-04 09:38:36 -0300

    atualizar
    pensei em uma ideia melhor e resolvi reiniciar

--- forms/form1/classjs/index.js
+++
@@ -1,8 +0,0 @@
-function eventoComecar(){
-    window.document.getElementById('config-display-modal').style.display = 'block'
-    window.document.getElementById('estilo-apresentacao').style.display = 'none'
-    setInterval(function(){
-        window.document.getElementById('config-display-form2').style.display = 'block'
-        window.document.getElementById('config-display-modal').style.display = 'none'
-    }, 1500)
-}
