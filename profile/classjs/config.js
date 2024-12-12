884a96d2a72b086992c0d5c997e69359a7a91a35
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-12 14:11:26 -0300

    enviar

--- profile/classjs/config.js
+++ profile/classjs/config.js
@@ -1,4 +1,5 @@
 setInterval(function(){
+    window.document.getElementById('condig-display-perfil').style.display = "block"
     window.document.getElementById('config-display').style.display = "none"
 },2500)
 
