1a5c9846020ad36856fcf5f5aa0a73748e072615
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-12 15:11:16 -0300

    enviar

--- profile/classjs/config.js
+++ profile/classjs/config.js
@@ -3,6 +3,17 @@ setInterval(function(){
     window.document.getElementById('config-display').style.display = "none"
 },2500)
 
-function mudarPerfil(){
-    
+function eventoVerificar(){
+    let nome = window.document.getElementById('nome').value
+    let telefone = window.document.getElementById('telefone').value
+    let sobre = window.document.getElementById('sobre').value
+    let file = window.document.getElementById('arquivo').value
+
+    const regEx_Telefone = /^\([0-9]{2}\) \+[0-9]{2} [0-9]{5}\-[0-9]{4}$/
+
+    if(regEx_Telefone.test(telefone) == true && nome.length >= 3 && sobre != ""){
+        alert('ok')
+    }else{
+        alert('erro')
+    }
 }
