59375b31cea5820b8838974559c6120f4d568174
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-20 15:52:44 -0300

    atualizar

--- complex-system/project1/classjs/index-padrao.js
+++ complex-system/project1/classjs/index-padrao.js
@@ -1,4 +1,18 @@
-setInterval(function(){
-    document.getElementById('config-display-form').style.display = 'block'
-    document.getElementById('display-container-carregamento').style.display = 'none'
-},2000)
+function tamanhoBody(){
+    if(window.innerWidth <= 1000){
+        document.getElementById('display-container-mensagem').style.display = 'block'
+        document.getElementById('config-display-form').style.display = 'none'
+    }else{
+        document.getElementById('display-container-mensagem').style.display = 'none'
+        document.getElementById('config-display-form').style.display = 'block'
+    }
+
+    if(window.innerHeight <= 618){
+        document.getElementById('display-container-mensagem').style.display = 'block'
+        document.getElementById('config-display-form').style.display = 'none'
+    }else{
+        document.getElementById('display-container-mensagem').style.display = 'none'
+        document.getElementById('config-display-form').style.display = 'block'
+    }
+
+}
