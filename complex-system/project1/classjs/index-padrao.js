559fff4550add2a32338c77460bd7acebf453c73
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-20 15:21:42 -0300

    atualizar

---
+++ complex-system/project1/classjs/index-padrao.js
@@ -0,0 +1,4 @@
+setInterval(function(){
+    document.getElementById('config-display-form').style.display = 'block'
+    document.getElementById('display-container-carregamento').style.display = 'none'
+},2000)
