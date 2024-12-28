e0118d3ed51161e0ba136317c48dd14862db2f99
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-28 10:26:20 -0300

    enviar

--- complex-system/project1/classjs/configjs.js
+++ complex-system/project1/classjs/configjs.js
@@ -2,3 +2,7 @@ function eventoClose(){
     window.document.getElementById('config-display-mensagem-erro').style.display = "none"
     window.document.getElementById('config-container-display').style.display = "block"
 }
+
+function eventoCamera(){
+    window.document.getElementById('img-produto').click()
+}
