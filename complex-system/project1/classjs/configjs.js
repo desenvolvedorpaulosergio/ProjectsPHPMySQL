da9c1a1fdebbde3b5b614ce1330ae20ad9ca70ce
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-28 07:53:35 -0300

    atualizar

---
+++ complex-system/project1/classjs/configjs.js
@@ -0,0 +1,4 @@
+function eventoClose(){
+    window.document.getElementById('config-display-mensagem-erro').style.display = "none"
+    window.document.getElementById('config-container-display').style.display = "block"
+}
