4eff851f28bc3bdb8edbbdcf60c2e74ad10602bc
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-30 10:13:20 -0300

    Enviar

--- complex-system/project1/classjs/configjs.js
+++ complex-system/project1/classjs/configjs.js
@@ -26,6 +26,8 @@ window.document.addEventListener('DOMContentLoaded', function(){
 function eventoJanelaCadastro(){
     window.document.getElementById('config-display-cadastro').style.display = "block"
     window.document.getElementById('config-display-excluir').style.display = "none"
+    window.document.getElementById('config-display-alterar').style.display = "none"
+
 
     window.document.getElementById('estilo-underline-cadastrar').style.textDecoration = 'underline'
     window.document.getElementById('estilo-underline-excluir').style.textDecoration = 'none'
@@ -35,10 +37,21 @@ function eventoJanelaCadastro(){
 function eventoJanelaExcluir(){
     window.document.getElementById('config-display-cadastro').style.display = "none"
     window.document.getElementById('config-display-excluir').style.display = "block"
+    window.document.getElementById('config-display-alterar').style.display = "none"
 
     window.document.getElementById('estilo-underline-cadastrar').style.textDecoration = 'none'
     window.document.getElementById('estilo-underline-excluir').style.textDecoration = 'underline'
     window.document.getElementById('estilo-underline-alterar').style.textDecoration = 'none'
+}
+
+function eventoJanelaAlterar(){
+    window.document.getElementById('config-display-cadastro').style.display = "none"
+    window.document.getElementById('config-display-excluir').style.display = "none"
+    window.document.getElementById('config-display-alterar').style.display = "block"
+
+    window.document.getElementById('estilo-underline-cadastrar').style.textDecoration = 'none'
+    window.document.getElementById('estilo-underline-excluir').style.textDecoration = 'none'
+    window.document.getElementById('estilo-underline-alterar').style.textDecoration = 'underline'
 }
 
 function eventoErro(){
@@ -49,5 +62,23 @@ function eventoErro(){
     }else{
         window.document.getElementById('cod-produto3').style.borderColor = 'black'
         window.document.getElementById('cod-produto3').style.color = 'black'
+    }
+}
+
+function eventoPreco(){
+    let precoCusto = document.getElementById('preco-custo3').value
+    let pecoVenda = document.getElementById('preco-venda3').value
+    if(precoCusto >= pecoVenda || precoCusto <= 0 || pecoVenda <= 0){
+        window.document.getElementById('preco-custo3').style.borderColor = 'red'
+        window.document.getElementById('preco-custo3').style.color = 'red'
+
+        window.document.getElementById('preco-venda3').style.borderColor = 'red'
+        window.document.getElementById('preco-venda3').style.color = 'red'
+    }else{
+        window.document.getElementById('preco-custo3').style.borderColor = 'black'
+        window.document.getElementById('preco-custo3').style.color = 'black'
+
+        window.document.getElementById('preco-venda3').style.borderColor = 'black'
+        window.document.getElementById('preco-venda3').style.color = 'black'
     }
 }
