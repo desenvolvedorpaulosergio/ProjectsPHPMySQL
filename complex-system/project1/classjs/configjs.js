932a6e53c19486275e83d2e72949e7102ed2ba77
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-30 07:54:09 -0300

    Enviar

--- complex-system/project1/classjs/configjs.js
+++ complex-system/project1/classjs/configjs.js
@@ -8,5 +8,23 @@ function eventoCamera(){
 }
 
 function eventoFechamento(){
-    window.document.getElementById('config-display-block-tabela').style.display = "none"
+    window.document.querySelectorAll('.config-display-block-tabela').forEach(function(elementos){
+        elementos.style.display = 'none'
+    })
+}
+
+function eventoVisualizar(){
+    window.document.querySelectorAll('.config-display-block-tabela').forEach(function(elementos){
+        elementos.style.display = 'block'
+    })
+}
+
+window.document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('estilo-underline-cadastrar').style.textDecoration = 'underline'
+})
+
+function eventoJanelaCadastro(){
+    window.document.getElementById('config-display-cadastro').style.display = "block"
+    // window.document.getElementById('config-display-mensagem-erro').style.display = "none"
+    // window.document.getElementById('config-display-mensagem-erro').style.display = "none"
 }
