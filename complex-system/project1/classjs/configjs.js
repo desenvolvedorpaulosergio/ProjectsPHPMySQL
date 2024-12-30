777d74917bfb10e782ac2601f41c5997563eace4
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-12-30 11:14:17 -0300

    enviar

--- complex-system/project1/classjs/configjs.js
+++ complex-system/project1/classjs/configjs.js
@@ -66,9 +66,29 @@ function eventoErro(){
 }
 
 function eventoPreco(){
-    let precoCusto = document.getElementById('preco-custo3').value
-    let pecoVenda = document.getElementById('preco-venda3').value
+    let precoCusto = document.getElementById('preco-custo').value
+    let pecoVenda = document.getElementById('preco-venda').value
+    
     if(precoCusto >= pecoVenda || precoCusto <= 0 || pecoVenda <= 0){
+        window.document.getElementById('preco-custo').style.borderColor = 'red'
+        window.document.getElementById('preco-custo').style.color = 'red'
+
+        window.document.getElementById('preco-venda').style.borderColor = 'red'
+        window.document.getElementById('preco-venda').style.color = 'red'
+    }else{
+        window.document.getElementById('preco-custo').style.borderColor = 'black'
+        window.document.getElementById('preco-custo').style.color = 'black'
+
+        window.document.getElementById('preco-venda').style.borderColor = 'black'
+        window.document.getElementById('preco-venda').style.color = 'black'
+    }
+}
+
+function eventoPreco3(){
+    let precoCusto3 = document.getElementById('preco-custo3').value
+    let pecoVenda3 = document.getElementById('preco-venda3').value
+
+    if(precoCusto3 >= pecoVenda3 || precoCusto3 <= 0 || pecoVenda3 <= 0){
         window.document.getElementById('preco-custo3').style.borderColor = 'red'
         window.document.getElementById('preco-custo3').style.color = 'red'
 
@@ -80,5 +100,71 @@ function eventoPreco(){
 
         window.document.getElementById('preco-venda3').style.borderColor = 'black'
         window.document.getElementById('preco-venda3').style.color = 'black'
+    }
+}
+
+function eventoQuantidade(){
+    let quantidade = window.document.getElementById('quantidade-produto').value
+    if(quantidade <= 0){
+        window.document.getElementById('quantidade-produto').style.color = 'red'
+        window.document.getElementById('quantidade-produto').style.borderColor = 'red'
+    }else{
+        window.document.getElementById('quantidade-produto').style.color = 'black'
+        window.document.getElementById('quantidade-produto').style.borderColor = 'black'
+    }
+}
+
+function eventoQuantidade2(){
+    let quantidade3 = window.document.getElementById('quantidade-produto3').value
+    if(quantidade3 <= 0){
+        window.document.getElementById('quantidade-produto3').style.color = 'red'
+        window.document.getElementById('quantidade-produto3').style.borderColor = 'red'
+    }else{
+        window.document.getElementById('quantidade-produto3').style.color = 'black'
+        window.document.getElementById('quantidade-produto3').style.borderColor = 'black'
+    }
+}
+
+function eventoCaractere(){
+    let nomeProduto = window.document.getElementById('nome-produto').value
+    if(nomeProduto.length < 4){
+        window.document.getElementById('nome-produto').style.color = 'red'
+        window.document.getElementById('nome-produto').style.borderColor = 'red'
+    }else{
+        window.document.getElementById('nome-produto').style.color = 'black'
+        window.document.getElementById('nome-produto').style.borderColor = 'black'
+    }
+}
+
+function eventoCaractere2(){
+    let categoria = window.document.getElementById('categoria-produto').value
+    if(categoria.length < 4){
+        window.document.getElementById('categoria-produto').style.color = 'red'
+        window.document.getElementById('categoria-produto').style.borderColor = 'red'
+    }else{
+        window.document.getElementById('categoria-produto').style.color = 'black'
+        window.document.getElementById('categoria-produto').style.borderColor = 'black'
+    }
+}
+
+function eventoRegEx(){
+    let categoria = window.document.getElementById('validade-produto').value
+    if(/^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/.test(categoria)){
+        window.document.getElementById('validade-produto').style.color = 'black'
+        window.document.getElementById('validade-produto').style.borderColor = 'black'
+    }else{
+        window.document.getElementById('validade-produto').style.color = 'red'
+        window.document.getElementById('validade-produto').style.borderColor = 'red'
+    }
+}
+
+function eventoRegEx2(){
+    let categoria = window.document.getElementById('validade-produto3').value
+    if(/^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/.test(categoria)){
+        window.document.getElementById('validade-produto3').style.color = 'black'
+        window.document.getElementById('validade-produto3').style.borderColor = 'black'
+    }else{
+        window.document.getElementById('validade-produto3').style.color = 'red'
+        window.document.getElementById('validade-produto3').style.borderColor = 'red'
     }
 }
